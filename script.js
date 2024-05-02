7d678e231c53b9296ee1c753400701d212a46e09
kayeepasion@gmail.com <kayeepasion@gmail.comq>
2024-05-02 23:11:48 +0800

    Add changes

--- script.js
+++ script.js
@@ -2,29 +2,29 @@ let count = 0;
 let timer;
 
 function updateCount() {
-document.getElementById('count').innerText = count;
+  document.getElementById('count').innerText = count;
 }
 
 document.getElementById('start').addEventListener('click', function() {
-timer = setInterval(function() {
-count++;
-updateCount();
-}, 500);
+  timer = setInterval(function() {
+    count++;
+    updateCount();
+  }, 500);
 });
 
 document.getElementById('pause').addEventListener('click', function() {
-clearInterval(timer);
+  clearInterval(timer);
 });
 
 document.getElementById('resume').addEventListener('click', function() {
-timer = setInterval(function() {
-count++;
-updateCount();
-}, 500);
+  timer = setInterval(function() {
+    count++;
+    updateCount();
+  }, 500);
 });
 
 document.getElementById('stop').addEventListener('click', function() {
-clearInterval(timer);
-count = 0;
-updateCount();
+  clearInterval(timer);
+  count = 0;
+  updateCount();
 });
